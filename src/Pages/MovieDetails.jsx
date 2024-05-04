8939567d50741b999777706e0a91d886741944ca
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2024-05-04 22:51:39 +0530

    ymd fix

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -33,7 +33,7 @@ export default function MovieDetails() {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Adding 1 because getMonth() returns zero-based month (0 for January, 11 for December)
-  const day = currentDate.getDate();
+  const day = currentDate.getDate().toString().padStart(2, '0');
   const ymd = `${year}${month}${day}`;
   const { id } = useParams();
   const urlParams = new URLSearchParams(window.location.search);
@@ -508,10 +508,13 @@ gsap.to('.recommds_list > div',{scrollTrigger:{trigger:'.recommds_list > div'}, 
                   </div>
                   }
                   {parseInt(tren.release_date.split('-').join('')) > parseInt(ymd)  &&
-                   <div id="remindmebt" onClick={()=>{document.getElementById('remindnotifycard').classList.toggle('animationdrag'); sendtodb(tren.id,tren.title,tren.release_date);}}>
+                   <div id="remindmebt" 
+                   onClick={()=>
+                   {document.getElementById('remindnotifycard').classList.toggle('animationdrag');
+                    sendtodb(tren.id,tren.title,tren.release_date);}}>
                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/></svg>
 
-Remind me
+Remind me 
                   </div>}
                 </div>
               </div>
