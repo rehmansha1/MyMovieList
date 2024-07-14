e6d2edaf9cf7b5d3f84bae8d59eaf9a91a6674f3
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2024-07-14 17:02:11 +0530

    fixed seasons bug

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -894,14 +894,15 @@ export default function MovieDetails() {
               )}
               {tren.seasons && (
                 <div className="seasonssection">
-                  <h1>Seasons</h1>
+                  <h1 >Seasons</h1>
 
                   <div className="seasonsarray">
                     <Swiper
                       spaceBetween={0}
                       modules={[Navigation, Scrollbar]}
                       slidesPerView={
-                        tren.seasons.length > 5 ? 5 : tren.seasons.length
+                        tren.seasons.length > 10 ? 5 : tren.seasons.length 
+                      
                       }
                       scrollbar={{ draggable: true }}
                       navigation={{
@@ -936,18 +937,28 @@ export default function MovieDetails() {
                           </svg>
                         </div>
                       </div>
-                      {console.log(tren.seasons.length - 1)}
+                      {console.log(tren.seasons )}
+
                       {tren.seasons.map((item, index) => {
-                        return (
-                          item.poster_path && (
+                        return ( 
+                           (
                             <SwiperSlide>
                               <div>
                                 {" "}
-                                <img
+                                {item.poster_path ? <img
                                   className="saimgs"
-                                  src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${item.poster_path}`}
-                                />
-                                <div className="saih1">{item.name}</div>
+                                  src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${item.poster_path}`  }
+                                /> : <div className="notimgs"><svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="white"
+                              height="54"
+                              viewBox="0 -960 960 960"
+                              width="54"
+                            >
+                              <path d="M558-84q-15 8-30.5 2.5T504-102l-60-120q-8-15-2.5-30.5T462-276q15-8 30.5-2.5T516-258l60 120q8 15 2.5 30.5T558-84Zm240 0q-15 8-30.5 2.5T744-102l-60-120q-8-15-2.5-30.5T702-276q15-8 30.5-2.5T756-258l60 120q8 15 2.5 30.5T798-84Zm-480 0q-15 8-30.5 2.5T264-102l-60-120q-8-15-2.5-30.5T222-276q15-8 30.5-2.5T276-258l60 120q8 15 2.5 30.5T318-84Zm-18-236q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z" />
+                            </svg></div>
+                            }
+                                <div className="saih1">{item.name? item.name:'null'}</div>
                               </div>
                             </SwiperSlide>
                           )
