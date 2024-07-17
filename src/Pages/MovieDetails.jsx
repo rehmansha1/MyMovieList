a88e4ae17335884452bff590c7e0a5b9e3b116de
rehmnshs <105535128+rehmnshs@users.noreply.github.com>
2024-07-17 12:08:24 +0530

    fixed review box and bgimg changes

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -833,15 +833,13 @@ const md = true
                         paramValue == "false") && (
                         <>
                           <div id="alreadywatched"   onClick={() => {
-                            setreviewbox(!reviewbox);
+                            setreviewbox(true);
                                 setreviewarray([
                                   `${tren.poster_path || tren.URL}`,
                                   tren.title || tren.name,
                                   tren.id,
                                 ]);
-                                const blackbox =
-                                  document.getElementById("blackboxiga");
-                                blackbox.classList.toggle("blackrevbox");
+                              
                               }}>
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -854,9 +852,9 @@ const md = true
                               <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
                             </svg>
                           </div>
-                          
+                          {!rt && !sentreview  &&  reviewbox && ( 
                           <div id="blackboxiga">
-                            {!rt && !sentreview && (
+                 
                               <>
                                 {" "}
                                 <div id="innerbbimgbox">
@@ -866,7 +864,7 @@ const md = true
                                   />
                                 </div>
                                 <div id="restbbbox">
-                                <div id="closeinfinite" onClick={()=>{document.getElementById('alreadywatched').click(); glassOut()} }>X</div>
+                                <div id="closeinfinite" onClick={()=>{document.getElementById('blackboxiga').style.animation = 'pulldown123123 0.5s both'; setTimeout(()=>setreviewbox(false),500) } }>X</div>
                                   <div id="titleofrevbox">
                                     {tren.name || tren.title}
                                   </div>
@@ -960,10 +958,11 @@ const md = true
                                   </div>
                                 </div>
                               </>
-                            )}
+                          
                             {!sentreview &&<div id="goingtosend"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg></div>}
                             {sentreview && <div id="checkbox"><svg xmlns="http://www.w3.org/2000/svg" height="74px" viewBox="0 -960 960 960" width="74px" fill="#e8eaed"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>{sentreview}</div>}
                           </div>
+                        )}
                         </>
                       )}
                     </div>
